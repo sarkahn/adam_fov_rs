8cfdd64c1708a9d2574e7a9a66bf708024b952d7
sark <sarkahn@gmail.com>
2026-06-02 14:46:33 -0600

    Update for bevy and ascii terminal 0.19 rc

--- examples/terminal.rs
+++ examples/terminal.rs
@@ -69,19 +69,19 @@ fn toggle_walls(
     q_cam: Single<&TerminalCamera>,
     q_term: Single<&TerminalTransform>,
 ) {
-    if mouse.just_pressed(MouseButton::Left) {
-        if let Some(p) = q_cam.cursor_world_pos() {
-            let Some(p) = q_term.world_to_tile(p) else {
-                return;
+    if mouse.just_pressed(MouseButton::Left)
+        && let Some(p) = q_cam.cursor_world_pos()
+    {
+        let Some(p) = q_term.world_to_tile(p) else {
+            return;
+        };
+        let size = IVec2::new(WIDTH as i32, HEIGHT as i32);
+        if p.cmpge(IVec2::ZERO).all() && p.cmplt(size).all() {
+            let i = index(p);
+            map.0[i] = match map.0[i] {
+                Tile::Floor => Tile::Wall,
+                Tile::Wall => Tile::Floor,
             };
-            let size = IVec2::new(WIDTH as i32, HEIGHT as i32);
-            if p.cmpge(IVec2::ZERO).all() && p.cmplt(size).all() {
-                let i = index(p);
-                map.0[i] = match map.0[i] {
-                    Tile::Floor => Tile::Wall,
-                    Tile::Wall => Tile::Floor,
-                };
-            }
         }
     }
 }
@@ -135,17 +135,19 @@ fn update_terminal_from_map(
 
     for x in 0..term.width() as i32 {
         for y in 0..term.height() as i32 {
-            let i = y as usize * WIDTH + x as usize;
+            let i = index(IVec2::new(x, y));
             if vision[i] {
                 let (c, col) = match walls[i] {
-                    Tile::Wall => ('#', color::GREEN),
-                    Tile::Floor => ('.', color::WHITE),
+                    Tile::Wall => ('#', color::css::GREEN),
+                    Tile::Floor => ('.', color::css::WHITE),
                 };
+                term.set_pivot(Pivot::LeftBottom);
                 term.put_char([x, y], c).fg(col);
             }
         }
     }
 
-    term.put_string([0, 0], "Click to toggle wall".clear_colors());
-    term.put_string([0, 1], "Scroll to change view range".clear_colors());
+    term.set_pivot(Pivot::LeftTop);
+    term.put_string([0, 0], "Click to toggle wall");
+    term.put_string([0, 1], "Scroll to change view range");
 }
